37808830334ae6e5f838400cb915aecbc47cce08
Tiger Nie <nhl0819@gmail.com>
2020-02-29 17:10:54 -0600

    Able to send photo from webcam to backend

--- api/app.py
+++ api/app.py
@@ -1,27 +1,31 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+from flask import Flask, jsonify, request, render_template
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-from api.TextDemo import MTS
+import numpy as np
+import cv2
+
+# from api.TextDemo import MTS
 
 
 def create_app():
 
     # Instantiate
-    app = Flask("MaskTextSpotterAPI", instance_relative_config=True)
-
-    # Add cors
-    CORS(app)
+    app = Flask(
+        "MaskTextSpotterAPI",
+        instance_relative_config=True,
+        static_folder="api/frontend/static",
+        template_folder="api/frontend",
+    )
 
     # proxy support
     app.wsgi_app = ProxyFix(app.wsgi_app)
 
     # Instantiate MaskTextSpotter
-    MTS()
+    # MTS()
 
     @app.route("/")
     def index():
-        return jsonify("hello world from Mask Textspotter")
+        return render_template("index.html")
 
     @app.route("/api")
     def index_api():
@@ -29,8 +33,14 @@ def create_app():
 
     @app.route("/api/upload", methods=["POST"])
     def upload():
-        imagefile = request.files.get('imagefile', '')
-        breakpoint()
+        image_blob = request.files.get("picture", "").read()
+        npimg = np.fromstring(image_blob, np.uint8)
+
+        img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+        import pdb
+
+        pdb.set_trace()
 
         return "/api/upload POST request received"
 
