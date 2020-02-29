3c0b83b6a04f819b827b1613d876c1ae34da5134
Ubuntu <ubuntu@ip-172-31-30-21.us-west-2.compute.internal>
2020-02-29 22:12:56 +0000

    working on image upload

--- api/app.py
+++ api/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from werkzeug.middleware.proxy_fix import ProxyFix
+
+from api.TextDemo import MTS
 
 
 def create_app():
@@ -14,8 +16,22 @@ def create_app():
     # proxy support
     app.wsgi_app = ProxyFix(app.wsgi_app)
 
+    # Instantiate MaskTextSpotter
+    MTS()
+
     @app.route("/")
     def index():
-        return jsonify("hello world!")
+        return jsonify("hello world from Mask Textspotter")
+
+    @app.route("/api")
+    def index_api():
+        return jsonify("MaskTextSpotter API")
+
+    @app.route("/api/upload", methods=["POST"])
+    def upload():
+        imagefile = request.files.get('imagefile', '')
+        breakpoint()
+
+        return "/api/upload POST request received"
 
     return app
